e634050aeb40ff4c31d5bb766b61b45fdb7a5122
kanji-world-sns <souichiro.doi.bussiness@gmail.com>
2025-08-12 13:34:05 +0900

    QRコードでアクセス可能になるdev2オプションを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { qrcode } from 'vite-plugin-qrcode'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), qrcode()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
